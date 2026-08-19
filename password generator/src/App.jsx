9cdf809b38f3d7220d64d4ac9392d3ff604f88eb
Samiksha gupta <samikshagupta0299@gmail.com>
2026-08-19 07:08:36 +0530

    making app function for password generator

--- password generator/src/App.jsx
+++ password generator/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState,useCallback } from 'react'
 import './App.css'
 
 function App() {
   const [length, setlength] = useState(8)
   const [numberAllowed,setNumberAllowed]=usestate(false);
-  const [charallowed,setcharallowed]=usestate(false)
+  const [charallowed,setcharallowed] = usestate(false)
   const [password,setpassword]=usestate("")
-  const passwordgenerator =() => {}
+  const passwordgenerator = useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charallowed) str += "!@#$%^&*()[]{},.+-*"
+    for(let i =1;i<=array.length;i++){
+      let char = Math.floor(Math.random() * str.length+1)
+      pass += str.charAt(char);
+
+    }
+    setpassword(pass)
+  },[length,numberAllowed,charallowed,setpassword])
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>password generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>test</div>
     </>
   )
 }
