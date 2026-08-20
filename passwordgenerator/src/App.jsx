30260fdc86fbb6c4e8e7478084d7090a797d9b53
Samiksha gupta <samikshagupta0299@gmail.com>
2026-08-20 13:00:26 +0530

    making ui of passwwordgenerator

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if(numberAllowed) str += "0123456789"
     if(charallowed) str += "!@#$%^&*()[]{},.+-*"
-    for(let i =1;i<=array.length;i++){
-      let char = Math.floor(Math.random() * str.length+1)
+    for(let i =1;i<=length;i++){
+      let char = Math.floor(Math.random() * str.length)
       pass += str.charAt(char);
 
     }
@@ -21,8 +21,25 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>test</div>
-      
+      <div className = "box">
+        <div className="section">
+            <div className='textsection'>
+              <input type='text' value={password} className='input' placeholder='password' readOnly ></input>
+              <button className='copybtn'>copy</button>
+          </div><br></br>
+          <div className='inputsection'>
+            <input type='range' min={5} max={100} value={length} className='slider' onChange={(e) =>{setlength(e.target.value)}} ></input>
+            <label>Length:{length}</label>
+            <input type='checkbox' defaultChecked={numberAllowed} id='numberInput' onChange={() =>{setNumberAllowed((prev) => !prev);}} ></input>
+            <label> Numbers</label>
+            <input type='checkbox' defaultChecked={numberAllowed} id='numberInput' onChange={() =>{setNumberAllowed((prev) => !prev);}} ></input>
+            <label> characters</label>
+          </div>
+
+        </div>
+
+        
+      </div>
     </>
 
   )
